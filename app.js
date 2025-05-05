617a1afc3b10eaa00d17334dced0a2e81167ff00
neethuss <neethuanitha5@gmail.com>
2025-05-05 16:41:19 +0530

    mongo format

--- app.js
+++ app.js
@@ -1,51 +1,48 @@
-const express = require('express')
-const path = require('path')
-const flash = require('connect-flash')
-const session = require('express-session')
-const mongoose = require('mongoose')
+const express = require("express");
+const path = require("path");
+const flash = require("connect-flash");
+const session = require("express-session");
+const mongoose = require("mongoose");
 
+require("dotenv").config();
 
-require('dotenv').config()
+const app = express();
 
-const app = express()
-
-
-app.set('view engine','ejs')
-app.set('views',path.join(__dirname,'views'))
-
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,  
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use((req, res, next) => {
   res.header("Cache-Control", "private, no-store, must-revalidate");
   next();
 });
 
-app.use(flash())
+app.use(flash());
 
-app.use('/',require('./routes/userRoutes'))
-app.use('/admin',require('./routes/adminRoutes'))
+app.use("/", require("./routes/userRoutes"));
+app.use("/admin", require("./routes/adminRoutes"));
 
 const port = process.env.PORT;
 
-const Banner = require('./models/bannerModel.js');
+const Banner = require("./models/bannerModel.js");
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect(process.env.MONGODB_URI)
+
   .then(() => {
     app.listen(port, () => {
       console.log(`Server running in http://localhost:${port}`);
@@ -53,4 +50,4 @@ mongoose.connect(process.env.MONGODB_URI, {
   })
   .catch((error) => {
     console.log("Error connecting to mongodb", error.message);
-  }); 
+  });
